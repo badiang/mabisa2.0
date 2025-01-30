843538caaf7e18ad934967747fcaabe166f98464
beejee0717 <bjbaculi123@gmail.com>
2025-01-31 06:41:43 +0800

    - fully integrated the functionalities for setup criteria - added db for sync

---
+++ db/barangay_assessment.sql
@@ -0,0 +1,47 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 29, 2025 at 01:54 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mabisa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barangay_assessment`
+--
+
+CREATE TABLE `barangay_assessment` (
+  `keyctr` int(11) NOT NULL,
+  `barangay_id` varchar(10) NOT NULL,
+  `last_modified` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `barangay_assessment`
+--
+
+INSERT INTO `barangay_assessment` (`keyctr`, `barangay_id`, `last_modified`) VALUES
+(1, '104201009', '2025-01-29 20:39:03'),
+(2, '104201018', '2025-01-12 13:54:36');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
